c9094ffb187118da5f82ff69644a55b1ba7db029
ticaso <ticaso999@gmail.com>
2024-04-14 18:03:31 -0500

    initial configuration

---
+++ Lab1/Lab1/Models/Entities/Profesion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1.Models.Entities;
+
+public partial class Profesion
+{
+    public int Id { get; set; }
+
+    public string? Nom { get; set; }
+
+    public string? Des { get; set; }
+
+    public virtual ICollection<Estudio> Estudios { get; set; } = new List<Estudio>();
+}
---
+++ Lab1/Lab1/Models/Entities/Telefono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1.Models.Entities;
+
+public partial class Telefono
+{
+    public string Num { get; set; } = null!;
+
+    public string? Oper { get; set; }
+
+    public int? Duenio { get; set; }
+
+    public virtual Persona? DuenioNavigation { get; set; }
+}
